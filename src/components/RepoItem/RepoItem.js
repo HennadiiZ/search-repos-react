b67181d3671989d0222310a1dbb889c43cbb5bd0
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-03-11 20:50:21 -0700

    fixing styles

--- src/components/RepoItem/RepoItem.js
+++ src/components/RepoItem/RepoItem.js
@@ -4,15 +4,19 @@ const RepoItem = (props) => {
   return (
     <li className={classes.repos_item}>
       <div className={classes.left_side}>
+
+
         <div className={classes.pic_wrapper}>
           <img src={props.pic} alt='pic'/>
         </div>
+
         <div className={classes.info_wrapper}>
           <h3>{props.name}</h3>
           <p>{props.author}</p>
           <p>{props.language}</p>
           <div>{props.description}</div>
         </div>
+        
       </div>
       <div className={classes.right_side}>
         <div className={classes.numbers_wrapper}>
